91e5a12e96675749aaea9901ddfba42f32815016
samtel <samtel@umich.edu>
2022-11-16 17:44:30 -0500

    done

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,13 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
-
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER)")
+    conn.commit()
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)", (0, 'Fluffle', 0, 3, 90, 100))
+    conn.commit()
     
 
 # TASK 2
@@ -58,14 +59,27 @@ def add_pets_from_json(filename, cur, conn):
     f.close()
     json_data = json.loads(file_data)
 
-    # THE REST IS UP TO YOU
-    pass
+    pet_id = 1
+    for pet in json_data:
+        name = pet["name"]
+        species = pet["species"]
+        age = pet["age"]
+        cuteness = pet["cuteness"]
+        aggressiveness = pet["aggressiveness"]
+        cur.execute("SELECT id FROM Species WHERE title = ?", (species,))
+        species_id = cur.fetchone()[0]
 
+        cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)", (pet_id, name, species_id, age, cuteness, aggressiveness))
+        pet_id += 1
+    conn.commit()
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    cur.execute("SELECT name FROM Patients WHERE aggressiveness <= ?", (aggressiveness,))
+    non_aggressive_pets_list = cur.fetchall()
+    non_aggressive_pets_list = [item[0] for item in non_aggressive_pets_list]
+    return non_aggressive_pets_list
 
 
 
